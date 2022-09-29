595563b33df6439e534d84ea27969ed046bd53e3
Romain Chabard <romainchabard@gmail.com>
2022-09-30 01:16:03 +0200

    [ADD] resultInfo [EDIT] userControllers/model

--- app/routers/question.router.js
+++ app/routers/question.router.js
@@ -10,14 +10,16 @@ router.get('/questions', handlerController(questionsController.getAllQuestions))
 /*OK*/
 router.post('/questions', handlerController(questionsController.createQuestion));
 /*OK*/
-router.get('/question/:id',handlerController(questionsController.getQuestionById));
+router.get('/question/:questionId',handlerController(questionsController.getQuestionById));
 /*OK*/
-router.patch('/question/:id',handlerController(questionsController.updateQuestion));
+router.patch('/question/:questionId',handlerController(questionsController.updateQuestion));
 /*OK*/
-router.delete('/question/:id',handlerController(questionsController.deleteQuestion));
-/*OK*/
-router.get('/question/:id/answers', handlerController(questionsController.getQuestionByIdAnswers));
+router.delete('/question/:questionId',handlerController(questionsController.deleteQuestion));
 
+/*recupere la question et ses answers*/
+router.post('/question/:questionId/answers', handlerController(questionsController.getQuestionByIdAnswers));
+
+// route pour poster une answer
 router.post('/question/:questionId/answer',tokenAuth.checkUser,handlerController(questionsController.getQuestionByIdAndCreateAnswer));
 
 module.exports = router;
--- app/routers/user.router.js
+++ app/routers/user.router.js
@@ -1,4 +1,4 @@
-const {Router, response} = require('express');
+const {Router} = require('express');
 const router = Router();
 
 const handlerController = require('../controllers/handlerController');
@@ -10,13 +10,16 @@ router.post('/sign-up', handlerController(usersController.signup));
 // This is the route for the validation of connexion
 router.post('/login', handlerController(usersController.login));
 
+// Route can't access if not logged
 /* This is the route for Find user By Id */
-router.get('/user/:id',tokenAuth.checkUser,handlerController(usersController.getUserById));
+router.post('/user',tokenAuth.checkUser,handlerController(usersController.getOneByPk));
 
-router.patch('/user/:id',handlerController(usersController.updateUser));
+router.patch('/user',tokenAuth.checkUser,handlerController(usersController.update));
 /*OK*/
-router.delete('/user/:id',handlerController(usersController.deleteUser));
+router.delete('/user',tokenAuth.checkUser,handlerController(usersController.delete));
 /*OK*/
-router.get('/users', handlerController(usersController.getAllUsers));
+
+// Route pour un admin
+router.post('/users',tokenAuth.checkUser, handlerController(usersController.getAll));
 
 module.exports = router;
